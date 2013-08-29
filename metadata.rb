afcf74b71809b117021f0d5d632d7bec0ec291a9
Edward Smith <edwardrsmi@gmail.com>
2013-08-28 19:24:56 -0700

    Initial commit.
    No functionality.  This is the initial commit of everything which was
    generated by the knife command.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ssh'
+maintainer       'Edward Smith'
+maintainer_email 'edwardrsmi@gmail.com'
+license          'MIT'
+description      'Installs/Configures ssh'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
