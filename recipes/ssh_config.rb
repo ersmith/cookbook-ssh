9c667a098d65637f01af3bced8d5c7403aefaa38
Edward Smith <edwardrsmi@gmail.com>
2013-08-29 21:14:14 -0700

    Updated the template location and fixed a few minor bugs.
    Moved the templates under templates/default and fixed a few typos
    including converting the keys in the attributes to symbols and
    replacing some single quotes with double quotes.

--- recipes/ssh_config.rb
+++ recipes/ssh_config.rb
@@ -1,4 +1,4 @@
-template '#{node[ssh][config_location]}/ssh_config' do
+template "#{node[:ssh][:config_location]}/ssh_config" do
   source 'ssh_config.erb'
   mode '0644'
   owner 'root'
--- recipes/sshd_config.rb
+++ recipes/sshd_config.rb
@@ -1,11 +1,11 @@
-template '#{node[ssh][config_location]}/sshd_config' do
+template "#{node[:ssh][:config_location]}/sshd_config" do
   source 'sshd_config.erb'
   mode '0644'
   owner 'root'
   group 'root'
   variables({
-    port => node[ssh][sshd][port],
-    password_authentication => node[ssh][sshd][password_authentication],
-    x11_forwarding => node[ssh][sshd][x11_forwarding]
+    :port => node[:ssh][:sshd][:port],
+    :password_authentication => node[:ssh][:sshd][:password_authentication],
+    :x11_forwarding => node[:ssh][:sshd][:x11_forwarding]
   })
 end
